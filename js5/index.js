049777bca5ad4b0a738b54ec6bf69964685efabd
smack-witch-doctor <160270838+smack-witch-doctor@users.noreply.github.com>
2024-04-24 19:09:20 +0200

    Add files via upload

---
+++ js5/index.js
@@ -0,0 +1,59 @@
+/*
+var names = [ 'john', 'marie', 'fred' ];
+console.log(typeof(names));objekt
+*/
+
+
+//Zadatak1 ---------------
+/*var brojcaniNiz =[1,2,3,4,5,6,7,8,9];
+var suma=0;
+for(x=0;x<brojcaniNiz.lenght;x++) {
+
+    if(typeof brojcaniNiz !='number')
+    { continue;}
+    suma+=brojcaniNiz[x];
+}
+console.log(suma);*/
+
+//Zadatak2-----------
+/*
+var matrix= [1,2,3],
+   ['john', 'jane', 'mike'];
+for(i=0;i<matrix.length;i++){
+   //console.log(matrix[i].join());
+ for(var j=0;j<matrix[i].length;j++)
+ {
+    console.log(matrix[i][j]);
+ }
+}
+*/
+//zadatak3-----------for of za nizove
+ /*var myNames = ['j','nui','sdfkigj' ];
+ for(var name1 of myNames){
+    console.log(name1);
+
+ }*/
+
+//zadatak4------ispisi svoje ime na kraju, prekini petlju nakon jane, sve prikazi abecednim redom
+/*var names= ['jphn', 'jane', 'mike',,'bob'];
+for(var name1 of names){
+    if(name1!==Number){
+        continue;
+    }
+    console.log(name1);
+
+}
+*/
+
+/*var names= ['jphn', 'jane', 'mike',,'bob'];
+
+console.log(names +'iva');
+*/
+//
+/* do{
+console.log(names);
+}while(names!=='jane');
+*/
+
+
+
